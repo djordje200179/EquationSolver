43eefec6f89fe780a0d9c71388268846e96da6e7
Đorđe Milanović <djordje200179@gmail.com>
2024-06-26 10:55:04 +0200

    Another test added

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -3,6 +3,7 @@ module Tests
 open EquationSolver.Solver
 open EquationSolver
 open Xunit
+open System
 
 [<Fact>]
 let ``Linear equation`` () =
@@ -19,3 +20,18 @@ let ``Linear equation`` () =
 
     Assert.Equal(1, zeroes.Length)
     Assert.Equal(-1.0, zeroes[0], 10)
+
+[<Fact>]
+let ``Discontinuous function`` () =
+    let config = {
+        Function = fun x -> 1.0 / x
+        Accuracy = 1e-10
+        Resolution = 1e-5
+    }
+
+    Assert.Throws<InvalidOperationException>(fun () ->
+        (-2.0, 2.0)
+        |> FindSolutions config
+        |> Seq.toList
+        :> obj
+    )

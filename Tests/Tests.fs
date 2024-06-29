890696cd29d67b5a1cac13c3f42249619a62396b
Đorđe Milanović <djordje200179@gmail.com>
2024-06-29 22:00:29 +0200

    Minor test improvements

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,12 +1,26 @@
 module Tests
 
 open EquationSolver.Solver
-open EquationSolver
 open Xunit
 open System
 
 [<Fact>]
-let ``Linear equation`` () =
+let ``Constant function has no solutions`` () =
+    let config = {
+        Function = fun x -> 1.0
+        Accuracy = 1e-10
+        Resolution = 1e-5
+    }
+
+    let zeroes = 
+        (-2.0, 2.0)
+        |> FindSolutions config
+        |> Seq.toList
+
+    Assert.Empty(zeroes)
+
+[<Fact>]
+let ``Linear equation has one solution`` () =
     let config = {
         Function = fun x -> x + 1.0
         Accuracy = 1e-10
@@ -22,7 +36,24 @@ let ``Linear equation`` () =
     Assert.Equal(-1.0, zeroes[0], 10)
 
 [<Fact>]
-let ``Discontinuous function`` () =
+let ``Quadratic equation has two solutions`` () =
+    let config = {
+        Function = fun x -> x * x - 1.0
+        Accuracy = 1e-10
+        Resolution = 1e-5
+    }
+
+    let zeroes = 
+        (-2.0, 2.0)
+        |> FindSolutions config
+        |> Seq.toList
+
+    Assert.Equal(2, zeroes.Length)
+    Assert.Equal(-1.0, zeroes.[0], 10)
+    Assert.Equal(1.0, zeroes.[1], 10)
+
+[<Fact>]
+let ``Discontinuous function causes exception`` () =
     let config = {
         Function = fun x -> 1.0 / x
         Accuracy = 1e-10
